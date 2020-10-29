8be1c6b80d92c18f86c834acf8d9e060c4d90e1a
megha <megha.nigam_cs19@gla.ac.in>
2020-10-29 22:25:10 +0530

    creating getter and setter method

--- src/main/Race.java
+++ src/main/Race.java
@@ -5,7 +5,10 @@ import thread.tortoise.Tortoise;
 
 public class Race {
     public static void main(String[] args) {
-
+        Rabbit rabbit = new Rabbit();
+        rabbit.getRabbit().start();
+        Tortoise tortoise=new Tortoise();
+        tortoise.start();
     }
     }
 
